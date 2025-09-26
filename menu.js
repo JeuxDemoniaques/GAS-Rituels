0ec4c748852c1428660472ce3da2aa26583877ba
Démon <demon@jeux-demoniaques.fr>
2025-09-26 16:10:59 +0200

    Add security popup to send confirmation mail

--- menu.js
+++ menu.js
@@ -1,11 +1,25 @@
 /***********************
  * Menu "Maintenance"  *
  ***********************/
+function confirmSendMail() {
+  const ui = SpreadsheetApp.getUi();
+  const response = ui.alert(
+    'Confirmation d\'envoi',
+    'Tu veux vraiment envoyer les mails de confirmation ?',
+    ui.ButtonSet.YES_NO
+  );
+
+  if (response === ui.Button.YES) {
+    sendMail();
+  }
+}
+
 function onOpen() {
   const ui = SpreadsheetApp.getUi();
   ui.createMenu('Nuit Démoniaques • Admin')
   .addItem('📤 Consolider les inscriptions', 'consolidation')
-  .addItem('📤 Envoyer les mails de confirmation', 'sendMail')
+  .addSubMenu(ui.createMenu('📨 Emails')
+    .addItem('📤 Envoyer les mails de confirmation', 'confirmSendMail')
   .addSeparator()
   .addItem('📐 Initialiser les feuilles', 'initSheets')
   // .addItem('🔍 Audit complet', 'auditComplet')
